e20a6d97ac1eb08cd61a8c74734262d7b8392c3a
CrestHawk <emailtomdavies@gmail.com>
2026-07-02 23:29:20 +0100

    Component Props Testing

--- hermes-client/src/App.tsx
+++ hermes-client/src/App.tsx
@@ -1,29 +1,21 @@
-import reactImg from "./assets/react-core-concepts.png";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[getRandomInt(reactDescriptions.length - 1)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>{description} React concepts you will need for almost any app you are going to build!</p>
-    </header>
-  );
-}
+import { Header } from "./components/Header/Header.tsx";
+import { CoreConcept } from "./components/CoreConcept/CoreConcept.tsx";
+import { CORE_CONCEPTS } from "./data.ts";
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
